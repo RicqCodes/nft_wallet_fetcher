8e1f09ddc5ecaa1c60525c3a5f48f495da7c817d
Ricqcodes <princenwakanma1996@gmail.com>
2024-02-25 22:19:42 +0100

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
-import { NFTDataProcessor } from "./NFTProcessor";
-import { COUNTER_CONTRACT_ADDRESSES } from "./services/constants";
+export { NFTDataProcessor } from "./NFTProcessor";
+// import { COUNTER_CONTRACT_ADDRESSES } from "./services/constants";
 
-const nftProcessor = new NFTDataProcessor(COUNTER_CONTRACT_ADDRESSES);
+// const nftProcessor = new NFTDataProcessor(COUNTER_CONTRACT_ADDRESSES);
 
-async function main() {
-  await nftProcessor.findOwnersOfNFTs();
-  await nftProcessor.convertToCSV();
-}
+// async function main() {
+//   await nftProcessor.findOwnersOfNFTs();
+//   await nftProcessor.convertToCSV();
+// }
 
-main();
+// main();
+
